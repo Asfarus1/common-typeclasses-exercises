c70c3112c83290d2788e4271a9df2465ec096bac
anton.matveev <anton.matveev@libertexgroup.com>
2021-05-27 13:33:41 +0200

    init

--- src/main/scala/fpcourse/Get.scala
+++ src/main/scala/fpcourse/Get.scala
@@ -24,37 +24,72 @@ object Get {
    * TODO 1
    * Consumes n bytes of input parsing no value.
    */
-  def skip(n: Int): Get[Unit] = ???
+  def skip(n: Int): Get[Unit] = Get[Unit] {
+    consume(n) fmap {
+      case Left(msg) => msg.asLeft
+      case Right((bytes, _)) => (bytes, ()).asRight
+    }
+  }
+
+  def consume(n: Int): List[Byte] => Either[String, (List[Byte], Array[Byte])] = {
+    bytes =>
+      bytes.splitAt(n) match {
+        case (read, tail) if read.length == n => (tail, read.toArray).asRight
+        case _ => "Insufficient input".asLeft
+      }
+  }
 
   /**
    * TODO 2
    * True if the input is fully consumed
    */
-  def isEmpty: Get[Boolean] = ???
+  def isEmpty: Get[Boolean] = Get[Boolean] {
+    bytes => (bytes, bytes.isEmpty).asRight
+  }
 
   /**
    * TODO 3
    * Reads one byte from input
    */
-  def getByte: Get[Byte] = ???
+  def getByte: Get[Byte] = Get[Byte] {
+    consume(1) fmap {
+      case Left(msg) => msg.asLeft
+      case Right((bytes, read)) => (bytes, read.head).asRight
+    }
+  }
 
   /**
    * TODO 4
    * Reads an Int from input using Big Endian order.
    */
-  def getIntBE: Get[Int] = ???
+  def getIntBE: Get[Int] = Get[Int] {
+    consume(4) fmap {
+      case Left(msg) => msg.asLeft
+      case Right((bytes, read)) => (bytes, bytesToIntUnsafe(read, ByteOrder.BIG_ENDIAN)).asRight
+    }
+  }
 
   /**
    * TODO 5
    * Reads an Int from input using Little Endian order.
    */
-  def getIntLE: Get[Int] = ???
+  def getIntLE: Get[Int] = Get[Int] {
+    consume(4) fmap {
+      case Left(msg) => msg.asLeft
+      case Right((bytes, read)) => (bytes, bytesToIntUnsafe(read, ByteOrder.LITTLE_ENDIAN)).asRight
+    }
+  }
 
   /**
    * TODO 6
    * Reads a String of n characters from input.
    */
-  def getString(n: Int): Get[String] = ???
+  def getString(n: Int): Get[String] = Get[String] {
+    consume(n) fmap {
+      case Left(msg) => msg.asLeft
+      case Right((bytes, read)) => (bytes, new String(read)).asRight
+    }
+  }
 
   /**
    * Helper function that turns four bytes into an Int. It doesn't check the
@@ -72,9 +107,18 @@ object Get {
    * Instance of monad error for Get.
    */
   implicit val monadGet: MonadError[Get, String] = new MonadError[Get, String] {
-    override def flatMap[A, B](fa: Get[A])(f: A => Get[B]): Get[B] = ???
+    override def flatMap[A, B](fa: Get[A])(f: A => Get[B]): Get[B] = Get[B] {
+      bytes =>
+        fa.run(bytes) match {
+          case Left(msg) => msg.asLeft
+          case Right(a) => f(a._2).run(bytes) match {
+            case Left(msg) => msg.asLeft
+            case Right(a1) => (bytes, a1._2).asRight
+          }
+        }
+    }
 
-    override def pure[A](x: A): Get[A] = ???
+    override def pure[A](x: A): Get[A] = Get(bytes => (bytes, x).asRight[String])
 
     override def tailRecM[A, B](a: A)(f: A => Get[Either[A, B]]): Get[B] = {
       Get { bytes =>
@@ -89,9 +133,21 @@ object Get {
       }
     }
 
-    override def raiseError[A](e: String): Get[A] = ???
+    override def raiseError[A](e: String): Get[A] = Get[A] {
+      _ => e.asLeft[(List[Byte], A)]
+    }
 
-    override def handleErrorWith[A](fa: Get[A])(f: String => Get[A]): Get[A] = ???
+    override def handleErrorWith[A](fa: Get[A])(f: String => Get[A]): Get[A] = Get[A] {
+      bytes =>
+        fa.run(bytes) match {
+          case Left(e) => f(e).run(bytes) match {
+            case Left(msg) => msg.asLeft
+            case Right(b) => (bytes, b._2).asRight[String]
+          }
+          case r => r
+        }
+    }
+
   }
 
   /**
@@ -104,11 +160,27 @@ object Get {
    * a generator of List[Byte], then sample it several times (e.g. 32)
    * and check that running both Gets yields the same result every time.
    */
-  implicit def eqGet[A: Eq]: Eq[Get[A]] = ???
+  implicit def eqGet[A: Eq]: Eq[Get[A]] = Eq.instance {
+    (a, b) =>
+      val bytegen = Gen.listOf[Byte](Gen.choose[Int](0, 255).map(_.toByte))
+
+      (1 to 32).forall(_ => {
+        val bytes = bytegen.sample.get
+        a.run(bytes) === b.run(bytes)
+      })
+  }
 
   /**
    * TODO 9
    * Monoid instance for Get.
    */
-  implicit def monoid[A: Monoid]: Monoid[Get[A]] = ???
+  implicit def monoid[A: Monoid]: Monoid[Get[A]] = new Monoid[Get[A]] {
+    override def empty: Get[A] = Monad[Get].pure(Monoid[A].empty)
+
+    override def combine(x: Get[A], y: Get[A]): Get[A] = for {
+      a <- x
+      b <- y
+    } yield a |+| b
+  }
+
 }
--- src/test/scala/fpcourse/Generators.scala
+++ src/test/scala/fpcourse/Generators.scala
@@ -1,6 +1,8 @@
 package fpcourse
 
 import org.scalacheck.{Arbitrary, Gen}
+import cats.implicits._
+
 
 trait Generators {
   /**
@@ -9,8 +11,13 @@ trait Generators {
    * No constraints on what the function does.
    * Simple solutions should be enough.
    */
-  implicit def arbFun[A](implicit arbA: Arbitrary[A]): Arbitrary[A => A] =
-    ???
+  implicit def arbFun[A](implicit arbA: Arbitrary[A]): Arbitrary[A => A] = {
+    val funGen = for {
+      v2 <- arbA.arbitrary
+      b <- Gen.oneOf(true, false)
+    } yield (v1: A) => v1//if (b) v1 else v2
+    Arbitrary(funGen)
+  }
 
   /**
    * TODO 11
@@ -19,9 +26,19 @@ trait Generators {
    * Some optional constraints if you want to spice things up:
    * - allow for both successful and failed return values to be produced
    * - when running the Get successfully, the remaining bytes should be
-   *   a suffix of the bytes passed as argument to the run function; that is,
-   *   you should simulate actual 'consumption' of the bytes.
+   * a suffix of the bytes passed as argument to the run function; that is,
+   * you should simulate actual 'consumption' of the bytes.
    */
-  implicit def arbGet[A](implicit arbA: Arbitrary[A]): Arbitrary[Get[A]] =
-    ???
+  implicit def arbGet[A](implicit arbA: Arbitrary[A]): Arbitrary[Get[A]] = {
+    val funGen = for {
+      v2 <- arbA.arbitrary
+      len <- Gen.posNum[Int]
+    } yield Get[A]{
+      Get.consume(len) fmap {
+        case Left(msg) => msg.asLeft[(List[Byte], A)]
+        case Right((bytes, _)) => (bytes, v2).asRight[String]
+      }
+    }
+    Arbitrary(funGen)
+  }
 }
